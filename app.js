3e58872e5c851e8d59118bc98a32840abc9f4788
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-07-26 09:53:36 -0300

    feat: create db connection

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const db = require('./db/connection');
 
 const port = 3000;
 
@@ -8,6 +8,17 @@ app.listen(port, function () {
   console.log(`O express está rodando na porta ${port}`);
 });
 
+//db connection
+db.authenticate()
+  .then(() => {
+    console.log('Conexão realizada com sucesso.');
+  })
+  .catch(err => {
+    console.error('Não foi possível conectar ao banco de dados:', err);
+  });
+
+
+//routes
 app.get('/', (req, res) => {
   res.send('Está funcionando');
 });
