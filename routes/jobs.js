1a867ae3fd43d5e645f570d653f518da4efd929d
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-07-29 16:20:19 -0300

    feat: create job preview page

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,6 +7,18 @@ router.get('/add', (req, res) => {
     res.render('add');
 });
 
+//detalhe da vaga
+router.get('/view/:id', (req, res) => {
+    Job.findOne({
+        where: { id: req.params.id }
+    }).then(job => {
+        res.render('view', {
+            job
+        });
+    }).catch(err => console.log(err));
+});
+
+//form da rota de criação de job	
 
 // add job via post
 router.post('/add', (req, res) => {
