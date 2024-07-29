8529aa4b19d253a2b2ece40f9c31b9b173541013
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-07-29 10:47:09 -0300

    feat: implemented handlebars

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
+const path = require('path');
 const db = require('./db/connection');
 const bodyParser = require('body-parser');
 const Job = require('./models/Job');
@@ -13,6 +15,14 @@ app.listen(port, function () {
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //db connection
 db.authenticate()
   .then(() => {
@@ -25,7 +35,7 @@ db.authenticate()
 
 //routes
 app.get('/', (req, res) => {
-  res.send('Está funcionando');
+  res.render('index');
 });
 
 //jobs routes
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
 
+//get job 
+router.get('/add', (req, res) => {
+    res.render('add');
+});
+
+
 // add job via post
 router.post('/add', (req, res) => {
     let { title, salary, company, description, email, new_job } = req.body;
